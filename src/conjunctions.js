178cd5d51264a0a319db1689153b4bac28efdf5f
ramasubramanian <ram.6383@gmail.com>
2016-06-19 23:41:46 +0530

    Tabs to spaces :-)

--- src/conjunctions.js
+++ src/conjunctions.js
@@ -10,27 +10,27 @@ var operators = new Operators();
 var Conjunctions = function() {};
 
 Conjunctions.prototype.condition = function(conjunctionDef) {
-	Assert.isDefined(conjunctionDef.lhs, "Missing lhs attribute in: " + JSON.stringify(conjunctionDef));
-	Assert.isDefined(conjunctionDef.rhs, "Missing rhs attribute in: " + JSON.stringify(conjunctionDef));
-	Assert.isDefined(conjunctionDef.op, "Missing op attribute in: " + JSON.stringify(conjunctionDef));
-	var lhsFn = valuesource.create(conjunctionDef.lhs);
-	var rhsFn = valuesource.create(conjunctionDef.rhs);
-	var op = operators.create(conjunctionDef.op);
-	return function(answers) {
-		var lhs = lhsFn(answers);
-		var rhs = rhsFn(answers);
-		return op(lhs, rhs);
-	};
+    Assert.isDefined(conjunctionDef.lhs, "Missing lhs attribute in: " + JSON.stringify(conjunctionDef));
+    Assert.isDefined(conjunctionDef.rhs, "Missing rhs attribute in: " + JSON.stringify(conjunctionDef));
+    Assert.isDefined(conjunctionDef.op, "Missing op attribute in: " + JSON.stringify(conjunctionDef));
+    var lhsFn = valuesource.create(conjunctionDef.lhs);
+    var rhsFn = valuesource.create(conjunctionDef.rhs);
+    var op = operators.create(conjunctionDef.op);
+    return function(answers) {
+        var lhs = lhsFn(answers);
+        var rhs = rhsFn(answers);
+        return op(lhs, rhs);
+    };
 };
 
 Conjunctions.prototype.create = function(conjunctionDef) {
-	var self = this;
-	var conjunction = self[conjunctionDef.type];
-	if(!conjunction) {
-		throw new Error("Unknown conjunction type: " + JSON.stringify(conjunctionDef));
-	} else {
-		return conjunction(conjunctionDef);
-	}	
+    var self = this;
+    var conjunction = self[conjunctionDef.type];
+    if(!conjunction) {
+        throw new Error("Unknown conjunction type: " + JSON.stringify(conjunctionDef));
+    } else {
+        return conjunction(conjunctionDef);
+    }   
 };
 
 exports.Conjunctions = Conjunctions;
